0d8a9a5bd3dab625dcd3eb5f6aa17b682b3f06ca
emmanuel mhango <emmanuellmhango@gmail.com>
2023-03-21 21:20:41 +0400

    Clear errors

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -5,9 +5,9 @@ class Recipe < ApplicationRecord
   validates :name, presence: true
   validates :name, uniqueness: { scope: :user_id }
   validates :description, presence: true
-  validates :preparation_time, presence: true
-  validates :preparation_time, numericality: { greater_than_or_equal_to: 0 }
-  validates :preparation_time, numericality: { only_integer: true }
+  validates :preparation_unit, presence: true
+  validates :preparation_unit, numericality: { greater_than_or_equal_to: 0 }
+  validates :preparation_unit, numericality: { only_integer: true }
   validates :cooking_time, presence: true
   validates :cooking_time, numericality: { greater_than_or_equal_to: 0 }
   validates :cooking_time, numericality: { only_integer: true }
